57563b145c4bd716b6ebfcf3c2a6add915fef33e
DP <DP@LAPTOP-2FR7R9FE>
2018-08-21 08:45:57 -0500

    Addtion and multiple program initial commit

---
+++ SampleApplicationForPOC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using SampleApplicationForPOC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleApplicationForPOC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Index(AdditionalViewModel model)
+        {
+            string isMuliplication = System.Configuration.ConfigurationManager.AppSettings["Multiplication"];
+            string isAdditional = System.Configuration.ConfigurationManager.AppSettings["Additional"];
+            if (isAdditional.ToUpper() == "TRUE")
+            {
+                model.Result = model.A + model.B;
+            }
+            
+            if (isMuliplication.ToUpper() == "TRUE")
+            {
+                model.Result = model.A * model.B;
+            }
+            return View(model);
+        } 
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ SampleApplicationForPOC/Models/AdditionalViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SampleApplicationForPOC.Models
+{
+    public class AdditionalViewModel
+    {
+        public double A
+        {
+            get;
+            set;
+        }
+        public double B
+        {
+            get;
+            set;
+        }
+        public double Result
+        {
+            get;
+            set;
+        }
+    }
+}
+
